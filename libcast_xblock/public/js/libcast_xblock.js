e2b0bea2466bd93cda875ad60854eca427c7d96b
Régis Behmo <regis@behmo.com>
2015-09-01 12:02:34 +0200

    Send events to tracking logs

--- libcast_xblock/public/js/libcast_xblock.js
+++ libcast_xblock/public/js/libcast_xblock.js
@@ -28,7 +28,6 @@ function LibcastXBlock(runtime, element, args) {
       });
 
       // Highlight current cue
-      // TODO
       for (var t = 0; t < tracks.length; t++) {
         tracks[t].addEventListener('cuechange', oncuechange);
       }
@@ -86,32 +85,46 @@ function LibcastXBlock(runtime, element, args) {
       }
     };
 
-    // TODO
-    //// Listen to events
-    //var listenToTime = function(eventName, logEventName) {
-      //player.on(event_name, function(){
-          //log(logEventName, {'new_time': parseInt(player.currentTime())});
-      //});
-    //};
-    //var listenTo = function(eventName, logEventName) {
-      //player.on(event_name, function(){
-          //log(logEventName);
-      //});
-    //};
-    //function log(eventName, data) {
-        //var logInfo = {
-          //course_id: args.course_id,
-          //video_id: args.video_id,
-        //};
-        //if (data) {
-          //$.extend(logInfo, data);
-        //}
-        //Logger.log(eventName, logInfo);
-    //}
-    //listenToTime('seeked', 'seek_video');
-    //listenToTime('ended', 'stop_video');
-    //listenToTime('pause', 'pause_video');
-    //listenToTime('play', 'play_video');
-    //listenTo('loadstart', 'load_video');
+    // Listen to events
+    var logTimeOnEvent = function(eventName, logEventName, currentTimeKey, data) {
+      player.on(eventName, function() {
+        logTime(logEventName, data, currentTimeKey);
+      });
+    };
+    var logTime = function(logEventName, data, currentTimeKey) {
+      data = data || {};
+      currentTimeKey = currentTimeKey || 'currentTime';
+      data[currentTimeKey] = parseInt(player.currentTime());
+      log(logEventName, data);
+    };
+    var logOnEvent = function(eventName, logEventName, data) {
+      data = data || {};
+      player.on(eventName, function() {
+          log(logEventName, data);
+      });
+    };
+    function log(eventName, data) {
+        var logInfo = {
+          course_id: args.course_id,
+          video_id: args.video_id,
+        };
+        if (data) {
+          $.extend(logInfo, data);
+        }
+        Logger.log(eventName, logInfo);
+    }
+
+    logTimeOnEvent('seeked', 'seek_video', 'new_time');
+    logTimeOnEvent('ended', 'stop_video');
+    logTimeOnEvent('pause', 'pause_video');
+    logTimeOnEvent('play', 'play_video');
+    logOnEvent('loadstart', 'load_video');
+    log('video_player_ready');
+    // Note that we have no show/hide transcript button, so there is nothing to
+    // log for these events
+
+    player.on('ratechange', function() {
+      logTime('speed_change_video', { newSpeed: player.playbackRate() });
+    });
   });
 }
