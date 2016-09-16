e844c82c913e2215ef0c9af9eedae3d29122db07
Richard Moch <richard.moch@gmail.com>
2016-09-16 09:42:14 +0200

    Remove Libcast dead code

--- libcast_xblock/libcast_xblock.py
+++ libcast_xblock/libcast_xblock.py
@@ -1,4 +1,4 @@
-"""XBlock for videos stored by Libcast."""
+"""XBlock for videos stored by Youtube."""
 
 import logging
 import pkg_resources
@@ -34,7 +34,7 @@ class LibcastXBlock(StudioEditableXBlockMixin, XBlock):
         scope=Scope.settings
     )
 
-    # The video ID is actually the resource ID, as defined by the libcast API
+    # Youtube ID
     video_id = String(
         scope=Scope.settings,
         help=ugettext_lazy('Fill this with the ID of the video found in the video uploads dashboard'),
@@ -69,31 +69,6 @@ class LibcastXBlock(StudioEditableXBlockMixin, XBlock):
     def resource_slug(self):
         return None if self.video_id is None else self.video_id.strip()
 
-    def transcript_root_url(self):
-        url = self.runtime.handler_url(self, 'transcript')
-        # url is suffixed with '?' in preview mode
-        return url.strip('?')
-
-    @XBlock.handler
-    def transcript(self, request, dispatch):
-        """
-        Proxy view for downloading subtitle files
-
-        <track> elements that point to external resources are not supported by
-        Chrome: https://code.google.com/p/chromium/issues/detail?id=472300
-        As a consequence, we need to download the subtitle file server-side.
-
-        Because srt subtitles are unsupported by Chrome, we need to convert
-        them to vtt format.
-        """
-        from videoproviders.api.libcast import get_vtt_content
-
-        subtitle_id = request.GET.get('id')
-        if not subtitle_id or not self.resource_slug:
-            return webob.Response(status=404)
-        caps = get_vtt_content(self.course_key_string, self.resource_slug, subtitle_id) or ""
-        return webob.Response(caps, content_type='text/vtt')
-
     def get_icon_class(self):
         """CSS class to be used in courseware sequence list."""
         return 'video'
@@ -124,54 +99,6 @@ class LibcastXBlock(StudioEditableXBlockMixin, XBlock):
             'is_studio': self.is_studio,
         }))
         fragment.add_content(content)
-
-    def get_libcast_content(self, fragment):
-        from videoproviders.api import libcast
-
-        template_content = self.resource_string("public/html/libcast.html")
-        template = Template(template_content)
-        messages = []  # tuple list
-        context = {
-            'display_name': self.display_name,
-            'video_id': self.resource_slug,
-            'transcript_root_url': self.transcript_root_url(),
-            'messages': messages,
-            'resource': {},  # To be provided by libcast
-        }
-        if not self.resource_slug:
-            messages.append(('warning', ugettext_lazy(
-                "You need to define a valid video ID. "
-                "Video IDs for your course can be found in the video upload"
-                " dashboard."
-            )))
-        else:
-            try:
-                context['resource'] = libcast.get_cached_resource_dict(self.course_key_string, self.resource_slug)
-                if not self.allow_download:
-                    context['resource'].pop('downloadable_files')
-            except libcast.MissingCredentials as e:
-                messages.append(('error', e.verbose_message))
-            except libcast.ClientError as e:
-                # Note that we may not log an exception here, because unicode
-                # messages cannot be encoded by the logger
-                logger.error(e.message)
-                messages.append(('error', ugettext_lazy("An unexpected error occurred.")))
-
-        content = template.render(Context(context))
-        fragment.add_content(content)
-
-        fragment.add_css(self.resource_string("public/css/style.css"))
-        # This hack requires us to hardcode the url to the css file.
-        # see no other way to proceed.  Loading static files directly from
-        # fun-apps/edx-platform is unconventional. It would be great to find a
-        # workaround.
-        fragment.add_css_url("/static/fun/js/vendor/videojs/video-js.min.css")
-        fragment.add_javascript(self.resource_string("public/js/libcast_xblock.js"))
-
-        fragment.initialize_js("LibcastXBlock", json_args={
-            'course_id': self.course_key_string,
-            'video_id': self.resource_slug,
-        })
 
     def get_youtube_content(self, fragment):
         # iframe element id
