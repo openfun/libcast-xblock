c9137d25505e610c3686b1b3a875c0cfe4024c58
Régis Behmo <regis@behmo.com>
2016-10-05 17:47:08 +0200

    Fix video thumbnail

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def package_data(pkg, roots):
 
 setup(
     name='libcast-xblock',
-    version='0.4.1',
+    version='0.4.2',
     description='XBlock for displaying videos hosted by Libcast',
     packages=[
         'libcast_xblock',
